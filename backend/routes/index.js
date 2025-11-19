68a0f45b1092f1d3e1e3d873c8c7102e2b6ae9e6
KmiloVm <kmilo7v@gmail.com>
2025-11-19 18:36:50 -0500

    Add token validation to user, employee, and department routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import authRoutes from './auth.routes.js';
 import userRoutes from './user.routes.js';
 import employeeRoutes from './employees.routes.js';
+import departmentRoutes from './department.routes.js';
+import validateToken from '../middlewares/validate-token.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
-router.use('/employees', employeeRoutes);
+router.use('/users', validateToken, userRoutes);
+router.use('/employees', validateToken, employeeRoutes);
+router.use('/departments', validateToken, departmentRoutes);
 
 export default router;
