2e6100f0216977af6a66addea9106ccc8ce1ce1b
KmiloVm <kmilo7v@gmail.com>
2025-11-19 17:00:55 -0500

    Add middleware to validate JWT tokens and update dependencies in package.json

---
+++ backend/middlewares/validate-token.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const validateToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+export default validateToken;
