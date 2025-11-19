e211a147485be4fc14ebed211a9d899a02d777fc
KmiloVm <kmilo7v@gmail.com>
2025-11-19 16:14:00 -0500

    Add department model

---
+++ backend/models/department.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const departmentSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Department = mongoose.model("Department", departmentSchema);
+
+export default Department;
