6faffa6cfcf687218ea2fe8be78c80ead72aac20
KmiloVm <kmilo7v@gmail.com>
2025-11-20 12:42:48 -0500

    Add Employee and User interfaces with their corresponding properties

--- frontend/src/app/core/models/employee.ts
+++ frontend/src/app/core/models/employee.ts
@@ -1,2 +1,18 @@
 export interface Employee {
+  id: number;
+  code: string;
+  firstName: string;
+  lastName1: string;
+  lastName2?: string;
+  departmentCode: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface EmployeeCreate {
+  code: string;
+  firstName: string;
+  lastName1: string;
+  lastName2?: string;
+  departmentCode: string;
 }
--- frontend/src/app/core/models/user.ts
+++ frontend/src/app/core/models/user.ts
@@ -1,2 +1,18 @@
 export interface User {
+  id: number;
+  username: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface UserLogin {
+  username: string;
+  password: string;
+}
+
+export interface UserRegister {
+  username: string;
+  password: string;
+  role?: string;
 }
